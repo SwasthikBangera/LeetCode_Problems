bf9cf5f76afd8428da86e03479a979c96920a9b6
Swasthik Bangera <37939646+SwasthikBangera@users.noreply.github.com>
2023-08-15 15:33:59 -0700

    Solution to prob 121

---
+++ Problem_121_Solution1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 15:30:52 2023
+
+@author: Swasthik Bangera
+
+Problem statement: You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the 
+future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
+
+            
