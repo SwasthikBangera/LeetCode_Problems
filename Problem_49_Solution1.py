00046daa6e56cf7fea2b87e3ceabdf0865e78777
Swasthik Bangera <37939646+SwasthikBangera@users.noreply.github.com>
2023-08-19 09:54:37 -0700

    Solution to problem 49

---
+++ Problem_49_Solution1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Swasthik Bangera
+
+Problem statement: Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the 
+original letters exactly once.
+
+
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
+
+            
